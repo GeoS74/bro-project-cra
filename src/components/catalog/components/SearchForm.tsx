f3c611328ccd4d244fed4396c0c15a017a71ad20
GeoS74 <gsirotkin@yandex.ru>
2023-01-20 17:38:53 +0500

    added plugin 'typescript-plugin-css-modules' and global.d.ts

--- src/components/catalog/components/SearchForm.tsx
+++ src/components/catalog/components/SearchForm.tsx
@@ -1,9 +1,8 @@
 import React from "react";
-// import styles from "./styles.module.css"
+import styles from "./styles.module.css"
 
 export default function SearchForm() {
-  // return <form onSubmit={onSubmit} className={styles.root}>
-  return <form onSubmit={onSubmit} className="bg-light border-primary">
+  return <form onSubmit={onSubmit} className={styles.root}>
       <input type="search" name="query-search" className="form-control me-sm-2" placeholder="Поиск позиций"/>
       <input type="submit" className="btn btn-outline-primary" value="Поиск"/>
   </form>
