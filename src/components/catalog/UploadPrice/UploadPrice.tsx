65ac3ad5c3f8397fb808615b41e607184a1c4ed4
GeoS74 <gsirotkin@yandex.ru>
2023-02-05 18:14:20 +0300

    added upload form

--- src/components/catalog/UploadPrice/UploadPrice.tsx
+++ src/components/catalog/UploadPrice/UploadPrice.tsx
@@ -1,23 +1,16 @@
 import { useState } from "react";
-import { useLoaderData } from "react-router-dom";
 
 import UploadPriceForm from "../UploadPriceForm/UploadPriceForm";
 import styles from "./styles.module.css"
 
 export default function UploadPrice() {
-  /*
-  * первый элемент массива исходных данных - бренды
-  * второй - поставщики
-  */
-  const loaderData = useLoaderData() as IRow[][];
-
   const [error, setError] = useState<string | undefined>(undefined);
   const [uploadState, setUploadState] = useState("new")
 
   return <div className={styles.root}>
     <h3>Загрузка прайса</h3>
 
-    {/* {error ? <span>{error}</span> : ""}
+    {error ? <span>{error}</span> : ""}
 
     {uploadState === "upload" ?
       <p>файл загружается ...</p> :
@@ -25,36 +18,36 @@ export default function UploadPrice() {
       uploadState === "end" ?
         <p>файл загружен.<span onClick={() => setUploadState("")}>Загрузить ещё?</span></p> :
 
-        <UploadPriceForm loaderData={loaderData} setError={setError} setUploadState={setUploadState} />
-    } */}
-
-
-
-
-
-
-
-    <ul className="nav nav-tabs" role="tablist">
-      <li className="nav-item" role="presentation">
-        <span className="nav-link" data-bs-toggle="tab" aria-selected="true" role="tab">Home</span>
-      </li>
-      <li className="nav-item" role="presentation">
-        <span className="nav-link active" data-bs-toggle="tab" aria-selected="false" role="tab">Profile</span>
-      </li>
-    </ul>
-    
-    <div id="myTabContent" className="tab-content">
-      <div className="tab-pane fade" id="home" role="tabpanel">
-        <p>first</p>
-      </div>
-      <div className="tab-pane fade active show" id="profile" role="tabpanel">
-        <p>second</p>
-      </div>
-    </div>
-
-
-
-
-
+        <UploadPriceForm setError={setError} setUploadState={setUploadState} />
+    }
   </div>
 }
+
+
+// function TestForm(){
+//   const [foo, setFoo] = useState("main")
+
+//   return <form>
+  //   <ul className="nav nav-tabs">
+  //   <li className="nav-item">
+  //     <span 
+  //       onClick={()=>setFoo("main")}
+  //       className={classNames("nav-link", foo==="main" ? "active":"")}>Home</span>
+  //   </li>
+  //   <li className="nav-item">
+  //     <span 
+  //     onClick={()=>setFoo("opt")}
+  //     className={classNames("nav-link", foo==="opt" ? "active":"")}>Profile</span>
+  //   </li>
+  // </ul>
+  
+  // <div id="myTabContent" className="tab-content">
+  // <div className={classNames("tab-pane fade", foo==="main" ? "active show":"")}>
+  //     <input type="text" name="bar" />
+  //   </div>
+  //   <div className={classNames("tab-pane fade", foo==="opt" ? "active show":"")}>
+  //   <input type="text" name="baz" />
+  //   </div>
+  // </div>
+//   </form>
+// }
--- src/components/catalog/UploadPriceForm/UploadPriceForm.tsx
+++ src/components/catalog/UploadPriceForm/UploadPriceForm.tsx
@@ -1,44 +1,19 @@
-import { useState } from "react";
 import config from "../../../config";
+import { UploadPriceFormTabPane } from "../UploadPriceFormTabPane/UploadPriceFormTabPane";
 
 type Props = {
-  /*
-  * первый элемент массива исходных данных loaderData - бренды
-  * второй - поставщики
-  */
-  loaderData: IRow[][],
   setError: React.Dispatch<React.SetStateAction<string | undefined>>,
   setUploadState: React.Dispatch<React.SetStateAction<string>>,
 }
 
-export default function UploadPriceForm({ loaderData, setError, setUploadState }: Props) {
+export default function UploadPriceForm({ setError, setUploadState }: Props) {
   return <form onSubmit={(event) => _onSubmit(event, setError, setUploadState)}>
-    <div className="form-group row col-sm-5">
-      <select name="brandId" className="form-select m-1">
-        <option value="0">Выберите бренд</option>
-        {_makeOptions(loaderData[0])}
-      </select>
 
-      <select name="providerId" className="form-select m-1">
-        <option value="0">Выберите поставщика</option>
-        {_makeOptions(loaderData[1])}
-      </select>
+    <legend className="mt-3 mb-4">Настройки загрузки файла Excel</legend>
 
-      <input type="file" name="file" className="form-control m-1" />
-    </div>
+    <UploadPriceFormTabPane />
 
-    <div className="form-group row col-sm-3">
-      начальная строка: <input type="text" name="startRow" placeholder="цена" defaultValue="1" />
-      конечная строка: <input type="text" name="endRow" placeholder="количество" />
-      <hr />
-    </div>
-
-    <MainFields />
-    <hr />
-    <OptionalFields />
-    <hr />
-
-    <input type="submit" className="btn btn-outline-primary" value="Загрузить прайс" />
+    <input type="submit" value="Загрузить прайс" className="btn btn-outline-light mt-4" />
   </form>
 }
 
@@ -51,7 +26,7 @@ function _onSubmit(
   event.preventDefault()
   setUploadState("upload");
 
-  fetch(`${config.catalog.back.host || ''}${config.catalog.back.port ? ':':''}${config.catalog.back.port || ''}/api/bridge/file/upload`, {
+  fetch(`${config.catalog.back.host || ''}${config.catalog.back.port ? ':' : ''}${config.catalog.back.port || ''}/api/bridge/file/upload`, {
     method: 'POST',
     body: new FormData(event.target as HTMLFormElement)
   })
@@ -74,47 +49,4 @@ function _onSubmit(
       setError("файл не загружен");
       console.log(error.message)
     })
-}
-
-function _makeOptions(brands: IRow[]) {
-  return brands.map((value, index) => {
-    return <option key={index} value={value.id} >
-      {value.title}
-    </option>
-  })
-}
-
-function MainFields() {
-  const [visible, setVisible] = useState(false)
-
-  return <div className="form-group row col-sm-3">
-    <label className="form-label mt-4" onClick={()=>setVisible(!visible)}>Основные данные</label>
-    {visible ?
-      <>
-        <input type="text" name="code" placeholder="код позиции" />
-        <input type="text" name="article" placeholder="артикул" />
-        <input type="text" name="title" placeholder="наименование" />
-        <input type="text" name="price" placeholder="цена" />
-        <input type="text" name="amount" placeholder="количество" />
-        <input type="text" name="profit" placeholder="наценка" />
-      </> : <></>
-    }
-  </div>
-}
-
-function OptionalFields() {
-  const [visible, setVisible] = useState(false)
-
-  return <div className="form-group row col-sm-3">
-    <label className="form-label mt-4" onClick={()=>setVisible(!visible)}>Дополнительные данные</label>
-    {visible ?
-      <>
-        <input type="text" name="manufacturer" placeholder="производитель" />
-      <input type="text" name="weight" placeholder="вес, кг" />
-      <input type="text" name="length" placeholder="длина, мм" />
-      <input type="text" name="width" placeholder="ширина, мм" />
-      <input type="text" name="height" placeholder="высота, мм" />
-      </> : <></>
-    }
-  </div>
 }
