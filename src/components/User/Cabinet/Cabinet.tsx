30e693c96802429ea9a9a178ad554cc20786a0ac
Aboltys754 <warwar754@mail.ru>
2023-02-26 21:40:43 +0500

    все не так. нада передалывать форму

--- src/components/User/Cabinet/Cabinet.tsx
+++ src/components/User/Cabinet/Cabinet.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import styles from "./styles.module.css"
 import classNames from "classnames"
 import Photo from "../Photo/Photo"
@@ -9,19 +9,29 @@ type Props = {
 }
 
 export default function Content({ user }: Props) {
+  // видно кнопки или нет
   const [editMode, setEditMode] = useState(false)
+  const [fornDataInfo, setFormDataInfo] = useState({position: ""})
+  useEffect (() => {
+    if (editMode === false) {
+      console.log(1)
+    }
+    
+  }, [editMode])
+  // ищем div таблицы с информацией
+  const divInformUser = useRef<HTMLDivElement>(null)
   
   return <div className={styles.root} >
     <h1>Личный кабинет</h1>
     <hr />
     <div className={styles.button}>
       <button onClick={() => {
-        setEditMode(!editMode)
-        console.log(editMode)
+        setEditMode(!editMode);
+        // console.log(formInformUser.current?.children[2].innerHTML)
         }}>редактировать профиль</button>
     </div>
     <div className={classNames(styles.content, "mt-4")}>    
-      <Photo user={user} editMode={editMode}/>
+      <Photo user={user} editMode={editMode} fornDataInfo={fornDataInfo}/>
       <div className="accordion" id={styles.accordion}>
 
         <div className="accordion-item">
@@ -31,11 +41,12 @@ export default function Content({ user }: Props) {
             </span>
           </h2>
           <div className="accordion-collapse">
-            <div className="accordion-body">
+            <div className="accordion-body" ref={divInformUser}>
               <p>email: {user.email}</p>
               <p>ранг: {user.rank}</p>
-              <p>должность: {user.position || "не указана"}</p>
-              <input type="text" value={"xsasca"}/>
+              <p className={classNames(editMode === false ? styles.disNon : styles.disBlok)}>должность: {user.position || "не указана"}</p>
+              <label htmlFor="position" className={classNames(editMode === true ? styles.disNon : styles.disBlok)}>должность</label>
+              <input type="text" className={classNames(editMode === true ? styles.disNon : styles.disBlok)} name="position"/>
             </div>
           </div>
         </div>
--- src/components/User/Photo/Photo.tsx
+++ src/components/User/Photo/Photo.tsx
@@ -2,48 +2,62 @@ import serviceHost from "../../../libs/service.host"
 import tokenManager from "../../../classes/TokenManager"
 import person from "../image/person.svg"
 import styles from "./styles.module.css"
+import classNames from "classnames"
 import { useRef, useState } from "react"
 
 type Props = {
     user: IUser,
-    editMode: boolean
+    editMode: boolean,
+    fornDataInfo: {position: string;}
   }
 
-export default function Photo({user, editMode}: Props) {
-    const [state, setState] = useState(false)
+export default function Photo({user, editMode, fornDataInfo}: Props) {
     // ссылка на input type=file
     const inputFile = useRef<HTMLInputElement>(null)
     // ссылка на input type=submit
     const inputSubmit = useRef<HTMLInputElement>(null)
     
-    const createInput = () => {
+    const clickInput = () => {
         inputFile?.current?.click();
     }
 
-    function foo() {
+    function clickSubmit() {
         inputSubmit?.current?.click();
     }
 
     async function submitForm (event: React.FormEvent<HTMLFormElement>)  {
         event.preventDefault();
-            const fd = new FormData(event.target as HTMLFormElement)
+        const fd = new FormData(event.target as HTMLFormElement)
+        fd.append("key1", "value1")
+        console.log(editMode)
+        console.log(fd)
+        if (editMode === false) {
             await fetch(`${serviceHost("informator")}/api/informator/user/photo`, {
                 method: 'PATCH',
                 headers: {
                     'Authorization': `Bearer ${tokenManager.getAccess()}`
                 },
                 body: fd
-                    }).finally(() => setState(!state))
+                    })
+        } else {
+            await fetch(`${serviceHost("informator")}/api/informator/user/photo`, {
+                method: 'PATCH',
+                headers: {
+                    'Authorization': `Bearer ${tokenManager.getAccess()}`
+                },
+                body: fd
+                    })
+        }
             
     }   
     
     return (
         <form className={styles.root} id="form_elem" onSubmit={submitForm}>
             {user.photo ? <img src={`${serviceHost('informator')}/api/informator/user/photo/${user.photo}`} 
-            loading="lazy" id={styles.imgPhoto} onClick={createInput}/> : <img src={person} loading="lazy" id={styles.imgPhoto} onClick={createInput}/>}
+            loading="lazy" id={styles.imgPhoto} onClick={clickInput}/> : <img src={person} loading="lazy" id={styles.imgPhoto} onClick={clickInput}/>}
             <div>
-                <input type="file" accept="image/*" ref={inputFile} className={styles.inputForm} name="photo" onChange={foo} data-foo ={state}/>
-                <input type="submit" id="inputSubmit" ref={inputSubmit}/>
+                <input type="file" accept="image/*" ref={inputFile} className={styles.inputForm} name="photo" onChange={clickSubmit}/>
+                <input type="submit" id="inputSubmit" ref={inputSubmit} className={classNames(editMode === true ? styles.disNon : styles.disBlok)}/>
             </div>
         </form>
             
