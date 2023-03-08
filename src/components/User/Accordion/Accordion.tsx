24a924f76e565b4eea24d439dc668fba9a4659ef
GeoS74 <gsirotkin@yandex.ru>
2023-03-08 13:33:59 +0300

    dev update user page

--- src/components/User/Accordion/Accordion.tsx
+++ src/components/User/Accordion/Accordion.tsx
@@ -2,42 +2,49 @@ import styles from "./styles.module.css"
 import classNames from "classnames"
 
 type Props = {
-  user: IUser
+  user: IUser,
+  editMode: boolean
 }
 
-export default function Accordion({user}: Props){
+export default function Accordion({ user, editMode }: Props) {
+  // console.log(user)
+
   return <div className={classNames(styles.root, "accordion")}>
 
-  <div className="accordion-item">
-    <h2 className="accordion-header" onClick={(event) => collapser(event)}>
-      <span className="accordion-button">
-        пользователь
-      </span>
-    </h2>
-    <div className="accordion-collapse">
-      <div className="accordion-body">
-        <p>email: {user.email}</p>
-        <p>ранг: {user.rank}</p>
-        <p>должность: {user.position || "не указана"}</p>
+    <div className="accordion-item">
+      <h2 className="accordion-header" onClick={(event) => collapser(event)}>
+        <span className="accordion-button">
+          пользователь
+        </span>
+      </h2>
+      <div className="accordion-collapse">
+        <div className="accordion-body">
+          <p>email: {user.email}</p>
+          <p>ранг: {user.rank}</p>
+          {/* <p>должность: {user.position || "не указана"}</p> */}
+          <p>должность: {editMode ?
+            <input type="text" name="position" defaultValue={user.position || ""} /> :
+            (user.position || "не указана")}
+          </p>
+        </div>
       </div>
     </div>
-  </div>
 
 
-  <div className="accordion-item">
-    <h2 className="accordion-header" onClick={(event) => collapser(event)}>
-      <span className="accordion-button collapsed">
-        доп. информация
-      </span>
-    </h2>
-    <div className="accordion-collapse collapse">
-      <div className="accordion-body">
-        lorem ipsum
+    <div className="accordion-item">
+      <h2 className="accordion-header" onClick={(event) => collapser(event)}>
+        <span className="accordion-button collapsed">
+          доп. информация
+        </span>
+      </h2>
+      <div className="accordion-collapse collapse">
+        <div className="accordion-body">
+          lorem ipsum
+        </div>
       </div>
     </div>
-  </div>
 
-</div>
+  </div>
 }
 
 function collapser(event: React.MouseEvent<HTMLHeadingElement, MouseEvent>) {
--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -2,7 +2,10 @@ import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 import Navigate from "../navigate/Navigate";
-import Cabinet from "./Cabinet/Cabinet"
+import Avatar from "./Avatar/Avatar"
+import Accordion from "./Accordion/Accordion"
+import styles from "./styles.module.css"
+import classNames from "classnames"
 
 export default function User() {
   const [user, setUser] = useState(useLoaderData() as IUser)
@@ -11,12 +14,44 @@ export default function User() {
   return <>
     <Navigate />
 
-    {editMode ?
-      <></>
-      // <EditForm user={user} setUser={setUser} editMode={editMode} setEditMode={setEditMode} />
-      : <>
+    <div className={styles.root} >
+      <h1>Личный кабинет</h1>
+      <hr />
 
-        <Cabinet user={user} />
-      </>}
+      <form onSubmit={event => _submit(event, editMode, setEditMode)}>
+        <div className={classNames(styles.content, "mt-4")}>
+
+          <div>
+            <Avatar userPhoto={user.photo} />
+
+            <input type="submit" className="btn btn-outline-light mt-4 mb-2"
+              value={editMode ? "Сохранить изменения" : "Редактировать профиль"}
+            />
+          </div>
+
+          <div><Accordion user={user} editMode={editMode} /></div>
+
+        </div>
+      </form>
+    </div>
   </>
+}
+
+function _submit(
+  event: React.FormEvent<HTMLFormElement>,
+  editMode: boolean,
+  setEditMode: React.Dispatch<React.SetStateAction<boolean>>
+  ){
+  event.preventDefault();
+  setEditMode(!editMode);
+
+  if(!editMode) return;
+
+  console.log("change")
+  console.log(editMode)
+
+  
+
+  const fd = new FormData(event.currentTarget)
+  console.log(fd.get('position'))
 }
