60908d8a7d5d24ab7caa95791106ecd6551c3faf
Aboltys754 <warwar754@mail.ru>
2023-03-10 21:20:04 +0500

    собрал обратно ответ. не знаю в каком формате отправлять

--- src/components/SettingAccess/Access/Access.tsx
+++ src/components/SettingAccess/Access/Access.tsx
@@ -3,27 +3,35 @@ import styles from "./styles.module.css"
 export default function Access({accessTable, nameUser}: {accessTable: object, nameUser: string}) {    
     const nameObjectAccess = Object.entries(accessTable)[0][0]
     const valueObjectAccess = Object.entries(accessTable)[0][1]
-    console.log(nameObjectAccess, valueObjectAccess)
-    if (valueObjectAccess === true) {
-        return (
-            <div className={styles.root}>
-                <p>{nameObjectAccess + ":"}</p>
-                <input type="radio" name={nameObjectAccess + nameUser} id={nameObjectAccess + nameUser + "1"} value="true" defaultChecked/>
-                <label htmlFor={nameObjectAccess + nameUser + "1"}>Включить</label>
-                <input type="radio" name={nameObjectAccess + nameUser} id={nameObjectAccess + nameUser + "2"} value="false"/>
-                <label htmlFor={nameObjectAccess + nameUser + "2"}>Выключить</label>
-            </div>
-        )
-    } else {
-        return (
-            <div className={styles.root}>
-                <p>{nameObjectAccess + ":"}</p>
-                <input type="radio" name={nameObjectAccess + nameUser} id={nameObjectAccess + nameUser + "1"} value="true"/>
-                <label htmlFor={nameObjectAccess + nameUser + "1"}>Включить</label>
-                <input type="radio" name={nameObjectAccess + nameUser} id={nameObjectAccess + nameUser + "2"} value="false" defaultChecked/>
-                <label htmlFor={nameObjectAccess + nameUser + "2"}>Выключить</label>
-            </div>
-        )
-    }
+    return (
+        <div className={styles.root}>
+            {valueObjectAccess ? 
+            <input type="checkbox" defaultChecked name={nameObjectAccess} value="true"/> : 
+            <input type="checkbox" name={nameObjectAccess}/>}            
+            <label htmlFor={nameObjectAccess + nameUser}>{nameObjectAccess}</label>
+        </div>
+    )
+
+    // if (valueObjectAccess === true) {
+    //     return (
+    //         <div className={styles.root}>
+    //             <p>{nameObjectAccess + ":"}</p>
+    //             <input type="radio" name={nameObjectAccess + nameUser} id={nameObjectAccess + nameUser + "1"} value="true" defaultChecked/>
+    //             <label htmlFor={nameObjectAccess + nameUser + "1"}>Включить</label>
+    //             <input type="radio" name={nameObjectAccess + nameUser} id={nameObjectAccess + nameUser + "2"} value="false"/>
+    //             <label htmlFor={nameObjectAccess + nameUser + "2"}>Выключить</label>
+    //         </div>
+    //     )
+    // } else {
+    //     return (
+    //         <div className={styles.root}>
+    //             <p>{nameObjectAccess + ":"}</p>
+    //             <input type="radio" name={nameObjectAccess + nameUser} id={nameObjectAccess + nameUser + "1"} value="true"/>
+    //             <label htmlFor={nameObjectAccess + nameUser + "1"}>Включить</label>
+    //             <input type="radio" name={nameObjectAccess + nameUser} id={nameObjectAccess + nameUser + "2"} value="false" defaultChecked/>
+    //             <label htmlFor={nameObjectAccess + nameUser + "2"}>Выключить</label>
+    //         </div>
+    //     )
+    // }
     
 }
--- src/components/SettingAccess/TableUsers/TableUsers.tsx
+++ src/components/SettingAccess/TableUsers/TableUsers.tsx
@@ -9,8 +9,40 @@ export default function TableUsers() {
     <div className={styles.root}>
         <h1>Настройки доступа</h1>
         <hr />
-        {users.map((key, index) => {
-            return <User key={index} nameUser={key.name} accessUser={key.access}/>            
-            })}
+        <div className={styles.div_root}>
+            <form onSubmit={event => upLoadAccessTable(event)}>
+                <input type="submit" className="btn btn-outline-light mt-4 mb-2" value={"Сохранить"}/>
+                <div>
+                    {users.map((key, index) => {
+                    return <User key={index} nameUser={key.name} accessUser={key.access}/>            
+                    })}
+                </div>
+            </form>            
+        </div>
+        
     </div>
 )}
+
+function upLoadAccessTable(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const accessTable: Array<object> = []
+    const userAccess = event.currentTarget.querySelectorAll('div.accordion')
+    userAccess.forEach((items, indexs) => {        
+        const tempArrayAccess: Array<object> = [] 
+        items.querySelectorAll('input').forEach((item, index) => {
+            item.value === "on" ?
+            tempArrayAccess[index] = {[item.name]: false} :
+            tempArrayAccess[index] = {[item.name]: true}
+        })
+        items.querySelectorAll('span').forEach((item) => {
+            accessTable[indexs] = {id: indexs, name: item.textContent, access: tempArrayAccess}
+        });
+    });
+    console.log(accessTable);
+    fetch ('localhost', {
+        method: 'PATCH',
+        headers: {
+            'Authorization': `Bearer`
+          }        
+    })
+}
