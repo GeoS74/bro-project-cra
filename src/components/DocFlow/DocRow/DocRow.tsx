026b3b9c5d76c4944ef39cab454121920367a257
GeoS74 <gsirotkin@yandex.ru>
2023-04-02 20:02:21 +0300

    ...

--- src/components/DocFlow/DocRow/DocRow.tsx
+++ src/components/DocFlow/DocRow/DocRow.tsx
@@ -6,14 +6,15 @@ import styles from "./styles.module.css"
 
 export default function DocRow(doc: IDoc) {
   return <div className={classNames(styles.root, "mt-2")}>
+    <small>{doc.directing?.title} / {doc.task?.title}</small>
 
-    <h5><Link to={`/docflow/${doc.id}`} className="nav-link">{doc.title}</Link></h5>
+    <h5 className="mt-2"><Link to={`/docflow/${doc.id}`} className="nav-link">{doc.title}</Link></h5>
 
     <ul>
       {doc.files.map(file => {
         return <li key={file.fileName + doc.id}>
           <a
-          className="text-muted"
+            className="text-muted"
             href={`${serviceHost('informator')}/api/informator/docflow/scan/${file.fileName}`}
             download={true}
           >{file.originalName}</a>
@@ -23,7 +24,5 @@ export default function DocRow(doc: IDoc) {
 
 
     <p>{doc.description}</p>
-
-    <small>{doc.directing?.title} / {doc.task?.title}</small>
   </div>
 }
