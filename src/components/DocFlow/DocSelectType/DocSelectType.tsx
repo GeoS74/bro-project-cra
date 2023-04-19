c559e32c0cf85f75ad9116ebe29114fccda53f61
geos <gsirotkin@yandex.ru>
2023-04-19 10:14:06 +0500

    fixed cancel button on select doctype

--- src/components/DocFlow/DocSelectType/DocSelectType.tsx
+++ src/components/DocFlow/DocSelectType/DocSelectType.tsx
@@ -3,6 +3,7 @@ import session from "../../../libs/token.manager"
 import styles from "./styles.module.css"
 import classNames from "classnames"
 import EditForm from "../EditForm/EditForm"
+import { ThemeContext } from "../../../contexts/ThemeContext/ThemeContext";
 
 type Props = {
   setShowForm: React.Dispatch<React.SetStateAction<boolean>>
@@ -12,9 +13,9 @@ type Props = {
 export default function DocSelectType({ setShowForm, addDoc }: Props) {
   const [typeDoc, setTypeDoc] = useState<DocType>()
 
-  if(typeDoc?.directing && typeDoc.task) {
+  if (typeDoc?.directing && typeDoc.task) {
     return <div className={classNames(styles.root, "mt-4")}>
-      <EditForm setShowForm={setShowForm} addDoc={addDoc} typeDoc={typeDoc}/>
+      <EditForm setShowForm={setShowForm} addDoc={addDoc} typeDoc={typeDoc} />
     </div>
   }
 
@@ -41,6 +42,10 @@ export default function DocSelectType({ setShowForm, addDoc }: Props) {
     </ul>
       : <></>}
 
-    <span className="btn btn-outline-light mt-2" onClick={() => setShowForm(false)}>Отмена</span>
+    <ThemeContext.Consumer>
+      {({ theme }) => (
+        <span className={classNames(`btn btn-outline-${theme === 'light' ? 'primary' : 'light'} mt-2`)} onClick={() => setShowForm(false)}>Отмена</span>
+      )}
+    </ThemeContext.Consumer>
   </div>
 }
