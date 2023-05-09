6d7db24dcc9fee82998aa457e24e405c9a8fbc8c
Aboltys754 <warwar754@mail.ru>
2023-05-09 23:00:42 +0500

    create tasks

--- src/components/UserPage/TaskPage/TaskPage.tsx
+++ src/components/UserPage/TaskPage/TaskPage.tsx
@@ -1,20 +1,28 @@
-import React from "react";
+import { useState } from "react";
 import styles from "./styles.module.css"
 import { Link } from "react-router-dom";
+import classNames from "classnames";
 
+import finder from "../../../libs/deep.finder"
+import session from "../../../libs/token.manager"
 import { ReactComponent as Icon0 } from "../image/0.svg";
 import { ReactComponent as Icon1 } from "../image/1.svg";
-import { ReactComponent as Icon2 } from "../image/2.svg";
-import { ReactComponent as Icon3 } from "../image/3.svg";
-import { ReactComponent as Icon4 } from "../image/4.svg";
-import { ReactComponent as Icon5 } from "../image/5.svg";
 
-export default function TaskPage({value, index, ListTasks, Path}: {value: string, index: number, ListTasks: IDoc[], Path: string}) {
-    const Icons = [{0: <Icon0 />}, {0: <Icon1 />}, {0: <Icon2 />}, {0: <Icon3 />}, {0: <Icon4 />}, {0: <Icon5 />}]    
+export default function TaskPage({setShowForm, value, index, ListTasks, Path}: {setShowForm: React.Dispatch<React.SetStateAction<boolean>>, value: string, index: number, ListTasks: IDoc[], Path: string}) {
+    const Icons = [{0: <Icon0 />}, {0: <Icon1 />}]
+   
+    session.subscribe('task');
+    if (value === "Мои поручения" || value === "Поручения мне")  {
+        return (
+            <Link to={Path} className={styles.root} state={{ListTasks, Path}}>
+                <div>{value}</div>
+                <div>{Icons[index][0]}</div>
+                <div>{ListTasks.length}</div>        
+            </Link>
+        )
+    }
     return (
-    <Link to={Path} className={styles.root} state={{ListTasks, Path}}>
-        <div>{value}</div>
-        <div>{Icons[index][0]}</div>
-        <div>{ListTasks.length}</div>        
-    </Link>
-)}
+        <button className={styles.root} onClick={() => setShowForm(true)}>
+            <div className={styles.createElement}>{value}</div>
+        </button>
+    )}
--- src/components/UserPage/TasksPage/TasksPage.tsx
+++ src/components/UserPage/TasksPage/TasksPage.tsx
@@ -7,19 +7,24 @@ import session from "../../../libs/token.manager"
 
 
 export default function TasksPage() {
-  return <></>
-//     const [docs, setDocs] = useState(useLoaderData() as IDoc[])    
-//     const bar = session?._me?.email
-//     const meTasks = docs.filter(user => user?.author === bar)
-//     const notMeTasks = docs.filter(user => user?.author !== bar)
-//     return (
-//         <div className={styles.root}>           
-//             <div className={styles.tasks}>            
-//                 <div className={styles.task}>
-//                     <TaskPage value={"Мои поручения"} index={0} ListTasks={meTasks} Path={"/userPage/listMeTasks"}/>
-//                     <TaskPage value={"Поручения мне"} index={1} ListTasks={notMeTasks} Path={"/userPage/listOtherTasks"}/>
-//                 </div>
-//             </div>           
-//         </div>    
-// )
+    const [docs, setDocs] = useState(useLoaderData() as IDoc[])
+    const [showForm, setShowForm] = useState(false);
+    const bar = session?._me?.email
+    const meTasks = docs.filter(user => user?.author.email === bar)
+    const notMeTasks = docs.filter(user => user?.author.email !== bar)
+    return (
+        <div className={styles.root}>
+          {showForm ?
+          <div>akfhafhkaejfk</div> :
+          <div className={styles.tasks}>            
+              <div className={styles.task}>
+                  <TaskPage value={"Мои поручения"} index={0} ListTasks={meTasks} Path={"/userPage/listMeTasks"} setShowForm={setShowForm}/>
+                  <TaskPage value={"Поручения мне"} index={1} ListTasks={notMeTasks} Path={"/userPage/listOtherTasks"} setShowForm={setShowForm}/>
+                  <TaskPage value={"Создать документ"} index={1} ListTasks={notMeTasks} Path={"/userPage/listOtherTasks"} setShowForm={setShowForm}/>
+              </div>
+          </div> 
+          }      
+                      
+        </div>    
+    )
 }
