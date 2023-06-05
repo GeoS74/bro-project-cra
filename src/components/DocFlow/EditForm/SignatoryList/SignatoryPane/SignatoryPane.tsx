63daa5816550ce3b9e900c99654dc0d83d9863c2
GeoS74 <gsirotkin@yandex.ru>
2023-06-05 22:44:03 +0300

    flow

--- src/components/DocFlow/EditForm/SignatoryList/SignatoryPane/SignatoryPane.tsx
+++ src/components/DocFlow/EditForm/SignatoryList/SignatoryPane/SignatoryPane.tsx
@@ -1,13 +1,39 @@
+import styles from "./styles.module.css"
+
 type Props = {
   signatory: IDocSignatory[]
 }
 
-export default function SignatoryPane({signatory}: Props) {
-  return <ul>
-  {signatory.map(s => (
-    <li key={s.uid}>{`${s.name} ${s.email}`}
-      <input type="hidden" name={`acceptor[${s.uid}]`} defaultValue={""} />
-    </li>
-  ))}
-</ul>
+export default function SignatoryPane({ signatory }: Props) {
+  return <ul className={styles.root}>
+    {signatory.map(s => (
+      <li
+        key={s.uid}
+        onMouseEnter={_showOptionalButton}
+        onMouseLeave={_showOptionalButton}
+      >{`${s.name} ${s.email}`}
+
+        <span hidden
+          onClick={(event) => {
+            if (!confirm('Удалить?')) {
+              return;
+            }
+            event.currentTarget.parentElement?.remove();
+            // if (docId) {
+            //   _delFile(docId, f.fileName)
+            // }
+          }}
+        ><small>удалить</small></span>
+
+        <input type="hidden" name={`acceptor[${s.uid}]`} defaultValue={""} />
+      </li>
+    ))}
+  </ul>
+}
+
+function _showOptionalButton(event: React.MouseEvent<HTMLLIElement, MouseEvent>) {
+  const optionalButton = event.currentTarget.querySelector('span') as HTMLElement | undefined;
+  if (optionalButton) {
+    optionalButton.hidden = !optionalButton.hidden;
+  }
 }
