72f26a2c21972457f775a05236da59ab467a9c21
geos <gsirotkin@yandex.ru>
2023-06-19 10:37:59 +0500

    fixed active pagination

--- src/components/Slider/Pagination/Pagination.tsx
+++ src/components/Slider/Pagination/Pagination.tsx
@@ -22,17 +22,32 @@ export default function Pagination({
         return <input key={i}
           className="form-check-input"
           type="radio"
-          checked={active >= i * countVisibleSlides &&
-            active < (i + 1) * countVisibleSlides ?
-            true : false}
+
+          checked={_isChecked(i, countVisibleSlides, countPaginations, countSlides, active)}
 
           onChange={() => {
             const active = (i + 1) * countVisibleSlides > countSlides ?
               countSlides - countVisibleSlides :
               i * countVisibleSlides;
+
             setActive(active)
           }}
         />
       })}
   </div>
+}
+
+function _isChecked(
+  index: number,
+  countVisibleSlides: number,
+  countPaginations: number,
+  countSlides: number,
+  active: number,
+) {
+  // если выполняется это условие, то достигнут предел прокрутки
+  if (active === countSlides - countVisibleSlides) {
+    return index + 1 === countPaginations
+  }
+
+  return active >= index * countVisibleSlides && active < (index + 1) * countVisibleSlides
 }
